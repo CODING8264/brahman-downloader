5ba1b59fce7d5f468d17cc216dd9541df3969150
CODING8264 <prashant.8264.mishra@gmail.com>
2026-02-16 13:04:00 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = false;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+// Use Railway dynamic port
+const port = parseInt(process.env.PORT, 10) || 8080;
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, () => {
+    console.log(`> Server listening on http://localhost:${port}`);
+  });
+});
